e9ff832e933d2bb3a144216264cfa7713ee34672
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-05-24 20:46:18 -0300

    feat: tail line in numbers

--- lib/parser.ex
+++ lib/parser.ex
@@ -1,18 +1,18 @@
 defmodule GenReport.Parser do
   def parse_file(filename) do
     filename
-    |> File.read()
-    |> handle_file()
-
-    # |> Stream.map(fn line -> parse_line(line) end)
+    |> File.stream!()
+    |> Enum.map(fn line -> parse_line(line) end)
   end
 
-  # defp parse_line(line) do
-  #   line
-  #   |> String.trim()
-  #   |> String.split(",")
-  # end
+  defp parse_line(line) do
+    [hd | tail] =
+      String.trim(line)
+      |> String.split(",")
 
-  defp handle_file({:ok, file_content}), do: file_content
-  defp handle_file({:err, _reason}), do: "Error while opening file"
+    [hd | Enum.map(tail, fn elem -> String.to_integer(elem) end)]
+  end
+
+  # defp handle_file({:ok, file_content}), do: file_content
+  # defp handle_file({:err, _reason}), do: "Error while opening file"
 end
