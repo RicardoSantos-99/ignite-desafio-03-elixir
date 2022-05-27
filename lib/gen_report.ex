8ad548aeaef86cd18c100ac2039814d108d9eab4
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-05-27 18:42:03 -0300

    feat: change report acc for return map

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -1,23 +1,45 @@
 defmodule GenReport do
+  alias GenReport.Parser
+
+  @available_names [
+    "Cleiton",
+    "Daniele",
+    "Danilo",
+    "Diego",
+    "Giuliano",
+    "Jakeliny",
+    "Joseph",
+    "Mayk",
+    "Rafael",
+    "Vinicius"
+  ]
+
   def build(filename) do
     filename
-    |> File.stream!()
+    |> Parser.parse_file()
     |> Enum.reduce(
       report_acc(),
-      fn line, report ->
-        [name, hours, day, month, year] = parse_line(line)
-        Map.put(report, name, report[name] + hours)
-      end
+      fn line, report -> sum_values(line, report) end
     )
   end
 
-  defp parse_line(line) do
-    [hd | tail] =
-      String.trim(line)
-      |> String.split(",")
-
-    [hd | Enum.map(tail, &String.to_integer/1)]
+  defp sum_values([name, hours, _day, _month, _year], report) do
+    Map.put(report, name, report[name] + hours)
   end
 
-  def report_acc, do: Enum.into(1..10, %{}, &{Integer.to_string(&1), 0})
+  def report_acc do
+    names = Enum.into(@available_names, %{}, &{&1, 0})
+    days = Enum.into(1..30, %{}, &{&1, 0})
+    hours = Enum.into(1..8, %{}, &{&1, 0})
+    months = Enum.into(1..12, %{}, &{&1, 0})
+    years = Enum.into(2016..2020, %{}, &{&1, 0})
+
+    %{
+      "names" => names,
+      "days" => days,
+      "hours" => hours,
+      "months" => months,
+      "years" => years
+    }
+  end
 end
--- lib/parser.ex
+++ lib/parser.ex
@@ -2,20 +2,17 @@ defmodule GenReport.Parser do
   def parse_file(filename) do
     filename
     |> File.stream!()
-    |> Enum.reduce(
-      %{},
-      fn line, report ->
-        [name, hours, day, month, year] = parse_line(line)
-        Map.put(report, name, report[name] + hours)
-      end
-    )
+    |> Stream.map(&parse_line/1)
   end
 
   defp parse_line(line) do
-    [hd | tail] =
-      String.trim(line)
-      |> String.split(",")
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> convert_list()
+  end
 
+  defp convert_list([hd | tail]) do
     [hd | Enum.map(tail, &String.to_integer/1)]
   end
 end
