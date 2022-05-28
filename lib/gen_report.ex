6613f366cabdf1f284e96901b6637db204f2f9ca
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-05-27 23:51:13 -0300

    feat: finish test

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -2,36 +2,41 @@ defmodule GenReport do
   alias GenReport.Parser
 
   @available_names [
-    "Cleiton",
-    "Daniele",
-    "Danilo",
-    "Diego",
-    "Giuliano",
-    "Jakeliny",
-    "Joseph",
-    "Mayk",
-    "Rafael",
-    "Vinicius"
+    "cleiton",
+    "daniele",
+    "danilo",
+    "diego",
+    "giuliano",
+    "jakeliny",
+    "joseph",
+    "mayk",
+    "rafael",
+    "vinicius"
   ]
 
   @available_months [
-    "Janeiro",
-    "Fevereiro",
-    "Março",
-    "Abril",
-    "Maio",
-    "Junho",
-    "Julho",
-    "Agosto",
-    "Setembro",
-    "Outubro",
-    "Novembro",
-    "Dezembro"
+    "janeiro",
+    "fevereiro",
+    "março",
+    "abril",
+    "maio",
+    "junho",
+    "julho",
+    "agosto",
+    "setembro",
+    "outubro",
+    "novembro",
+    "dezembro"
   ]
+
+  def build do
+    {:error, "Insira o nome de um arquivo"}
+  end
 
   def build(filename) do
     filename
     |> Parser.parse_file()
+    |> IO.inspect()
     |> Enum.reduce(
       report_acc(),
       fn line, report -> sum_values(line, report) end
@@ -39,36 +44,34 @@ defmodule GenReport do
   end
 
   defp sum_values(
-         [name, hours, day, month, year],
+         [name, hours, _day, month, year],
          report
        ) do
-    IO.inspect(report)
+    all_hours = report["all_hours"]
+    hours_per_month = report["hours_per_month"]
+    hours_per_year = report["hours_per_year"]
 
-    name_list = report["all_hours"]
-    year_list = report["hours_per_year"]
-    months_index = report["hours_per_month"]
-
-    all_hours = Map.put(name_list, name, name_list[name] + hours)
+    all_hours = Map.put(all_hours, name, all_hours[name] + hours)
 
     hours_per_month =
       Map.put(
-        months_index,
+        hours_per_month,
         name,
         Map.put(
-          months_index[name],
+          hours_per_month[name],
           month,
-          months_index[name][month] + hours
+          hours_per_month[name][month] + hours
         )
       )
 
     hours_per_year =
       Map.put(
-        year_list,
+        hours_per_year,
         name,
         Map.put(
-          year_list[name],
+          hours_per_year[name],
           year,
-          year_list[name][year] + hours
+          hours_per_year[name][year] + hours
         )
       )
 
@@ -81,16 +84,13 @@ defmodule GenReport do
 
   def report_acc do
     all_hours = Enum.into(@available_names, %{}, &{&1, 0})
-    hours_per_month = Enum.into(1..12, %{}, &{&1, 0})
     hours_per_year = Enum.into(2016..2020, %{}, &{&1, 0})
-
-    hours_per_month = Enum.into(@available_names, %{}, &{&1, hours_per_month})
-    hours_per_year = Enum.into(@available_names, %{}, &{&1, hours_per_year})
+    hours_per_month = Enum.into(@available_months, %{}, &{&1, 0})
 
     %{
       "all_hours" => all_hours,
-      "hours_per_month" => hours_per_month,
-      "hours_per_year" => hours_per_year
+      "hours_per_year" => Enum.into(@available_names, %{}, &{&1, hours_per_year}),
+      "hours_per_month" => Enum.into(@available_names, %{}, &{&1, hours_per_month})
     }
   end
 end
