548c80eb4fa10743e5e288752a3d57de72de24dc
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-05-27 22:38:13 -0300

    feat: hours per year

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -14,6 +14,21 @@ defmodule GenReport do
     "Vinicius"
   ]
 
+  @available_months [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro"
+  ]
+
   def build(filename) do
     filename
     |> Parser.parse_file()
@@ -24,21 +39,58 @@ defmodule GenReport do
   end
 
   defp sum_values(
-         [name, hours, _day, _month, _year],
-         %{
-           "all_hours" => names
-         } = _report
+         [name, hours, day, month, year],
+         report
        ) do
-    all_hours = Map.put(names, name, names[name] + hours)
+    IO.inspect(report)
+
+    name_list = report["all_hours"]
+    year_list = report["hours_per_year"]
+    months_index = report["hours_per_month"]
+
+    all_hours = Map.put(name_list, name, name_list[name] + hours)
+
+    hours_per_month =
+      Map.put(
+        months_index,
+        name,
+        Map.put(
+          months_index[name],
+          month,
+          months_index[name][month] + hours
+        )
+      )
+
+    hours_per_year =
+      Map.put(
+        year_list,
+        name,
+        Map.put(
+          year_list[name],
+          year,
+          year_list[name][year] + hours
+        )
+      )
 
     %{
-      "all_hours" => all_hours
+      "all_hours" => all_hours,
+      "hours_per_month" => hours_per_month,
+      "hours_per_year" => hours_per_year
     }
   end
 
-  defp report_acc do
+  def report_acc do
+    all_hours = Enum.into(@available_names, %{}, &{&1, 0})
+    hours_per_month = Enum.into(1..12, %{}, &{&1, 0})
+    hours_per_year = Enum.into(2016..2020, %{}, &{&1, 0})
+
+    hours_per_month = Enum.into(@available_names, %{}, &{&1, hours_per_month})
+    hours_per_year = Enum.into(@available_names, %{}, &{&1, hours_per_year})
+
     %{
-      "all_hours" => Enum.into(@available_names, %{}, &{&1, 0})
+      "all_hours" => all_hours,
+      "hours_per_month" => hours_per_month,
+      "hours_per_year" => hours_per_year
     }
   end
 end
